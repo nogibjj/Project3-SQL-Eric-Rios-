077b23f523b32b6976c68cb0802b1a05b9a0c96f
Eric Ríos <70504872+EricR401S@users.noreply.github.com>
2022-11-14 14:35:37 +0000

    new notes

--- query_builder.py
+++ query_builder.py
@@ -4,6 +4,7 @@ query1 = 'SELECT Name, Platform FROM games LIMIT 5'
 query2 = 'SELECT Name, Platform, Year_of_Release FROM games WHERE Year_of_Release > 2000 LIMIT 10'
 query3 = 'SELECT Name, Platform, Year_of_Release FROM games WHERE Year_of_Release > 2000 AND Platform = "PS2" LIMIT 20'
 
+
 query4 = 'INSERT INTO games(Name, Platform, Year_of_Release,Genre, Publisher, Global_Sales) VALUES (?, ?, ?, ?, ?, ?)'
 
 connection = sqlite3.connect('database.db')
@@ -19,6 +20,10 @@ with open('Video_Games_Sales_2016 - Copy.csv') as file:
 
 for row in vg:
     cursor.execute(query4, row)
+    # the first elements in the row
+    # Assert : First Row : ['Name', 'Platform', 'Year_of_Release', 'Genre', 'Publisher', 'Global_Sales'], has to be its own file, 
+    # asserts for all queries
+    # Test_File, is run by Github Actions, in make test
     
 connection.commit()
 
@@ -28,3 +33,15 @@ for i in cursor.execute(query3):
 
     print(i)
 
+# Missing : Github actions 
+# test file 
+# external connections to database--Recc : Postgres-Alfredo's/Rec:BigQuery-Google/AWS-RDS/
+# Alfredo - Demo Github - Postgres
+# Secrets, passwords, connections to database (PG/BQ)
+
+
+#Project 4
+# API to connect to database
+# Microservice : Do something specific, and do it well - quey the database
+# Streamlit/Fast API - Frontend, a web app where users can input what they want - Noah - Continuous Deployment
+# Apprunner - AWS - Deploy a web app - Noah, on linkedin, repos
