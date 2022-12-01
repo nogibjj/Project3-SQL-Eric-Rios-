e65235481356f32979513eec7d309e56840fbd4a
Eric Ríos <70504872+EricR401S@users.noreply.github.com>
2022-12-01 04:46:46 +0000

    final additions

--- query_builder.py
+++ query_builder.py
@@ -13,17 +13,14 @@ cursor = connection.cursor()
 
 from csv import reader 
 vg = []
-with open('Video_Games_Sales_2016 - Copy.csv') as file:
+with open('Video_Games_Sales_2016 - Copy.csv', encoding='utf-8') as file:
     csv_reader = reader(file)
     for row in csv_reader:
         vg.append(row)
 
 for row in vg:
     cursor.execute(query4, row)
-    # the first elements in the row
-    # Assert : First Row : ['Name', 'Platform', 'Year_of_Release', 'Genre', 'Publisher', 'Global_Sales'], has to be its own file, 
-    # asserts for all queries
-    # Test_File, is run by Github Actions, in make test
+
     
 connection.commit()
 
@@ -32,16 +29,3 @@ connection.commit()
 for i in cursor.execute(query3):
 
     print(i)
-
-# Missing : Github actions 
-# test file 
-# external connections to database--Recc : Postgres-Alfredo's/Rec:BigQuery-Google/AWS-RDS/
-# Alfredo - Demo Github - Postgres
-# Secrets, passwords, connections to database (PG/BQ)
-
-
-#Project 4
-# API to connect to database
-# Microservice : Do something specific, and do it well - quey the database
-# Streamlit/Fast API - Frontend, a web app where users can input what they want - Noah - Continuous Deployment
-# Apprunner - AWS - Deploy a web app - Noah, on linkedin, repos
